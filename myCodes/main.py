91e7cb061968a23b0c62e68104958b5d670a0d21
haoczhao <haoczhao@student.unimelb.edu.au>
2022-10-21 23:34:25 +0800

    adding codes

---
+++ myCodes/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+path = r'D:\= ='
+
+# data = pd.read_csv(path + os.sep + "task1.csv")
+
+for i in range(1, 51):
+    os.mkdir(path + os.sep + "helloWorld" + str(i))
+
+# for i in range(1, 51):
+#     for j in range(1, 51):
+#         f = open(path + os.sep + "helloWorld" + str(i) + os.sep + "helloWorld" + str(j) + ".txt", "w")
+#         f.write("hello world niu bi")
+
+a = 1
+print(a)
+
+
+print(2)
